d38421c849b6cc1b6c9fdd749d53f935040af341
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-04-21 04:27:07 -0430

    Añadir paleta de comandos

--- Data.py
+++ Data.py
@@ -1,10 +1,53 @@
-def crear_cita(lista, cedula, nombre, apellido, edad, sexo, numero_cita):
+#_*_coding:UTF-8_*_
+from tkinter import *
+
+# Colores:
+color_text           = "#FFFFFF"
+color_dark_primary   = "#455A64"
+color_primary        = "#607D8B"
+color_accent         = "#00BCD4"
+color_primary_text   = "#212121"
+color_secundary_text = "#727272"
+
+
+# Lista de Medicamentos:
+
+lista_tratamientos = [
+    { 'nombre': 'Vacio', 'precio': 0 },
+    { 'nombre': 'Restauración en Amalgama', 'precio': 5000},
+    { 'nombre': 'Restauración en Resina', 'precio': 7000 },
+    { 'nombre': 'Endodoncia', 'precio': 8000 },
+    { 'nombre': 'Exodoncia Simple', 'precio': 9000 },
+    { 'nombre': 'Exodoncia Compleja', 'precio': 102789 }
+]
+
+# Añadir nuevo paciente a la lista.
+def crear_cita(lista, cedula, nombre, apellido, edad, sexo, numero_cita, tratamiento):
     paciente = {
+        'cedula': cedula,
         'nombre': nombre,
         'apellido': apellido,
         'edad': edad,
         'sexo': sexo,
-        'numero_cita': numero_cita
+        'numero_cita': numero_cita,
+        'tratamiento': tratamiento
     }
-    
+
+    for tratamiento_item in lista_tratamientos:
+        if tr['nombre'] == tratamiento:
+            paciente['costo_tratamiento'] = tratamiento_item['precio']
+
+    total_cita(paciente, numero_cita)
+
     lista.append(paciente)
+
+# Calculo del pago de la cita.
+def total_cita(paciente, numero_cita):
+    if numero_cita <= 3:
+        paciente['total_cita'] = 6000
+    elif numero_cita >= 4 and numero_cita <= 5:
+        paciente['total_cita'] = 4500
+    elif numero_cita > 5 and numero_cita <= 8:
+        paciente['total_cita'] = 3000
+    else:
+        paciente['total_cita'] = 2000
--- Gui.py
+++ Gui.py
@@ -1,5 +1,37 @@
+from tkinter import *
+
+# Colores:
+color_text           = "#FFFFFF"
+color_dark_primary   = "#455A64"
+color_primary        = "#607D8B"
+color_accent         = "#00BCD4"
+color_primary_text   = "#212121"
+color_secundary_text = "#727272"
+
 def mostrar(ventana):
     ventana.deiconify()
-    
+
 def esconder(ventana):
     ventana.withdraw()
+
+def mostrar_lista_espera(lista, ventana):
+
+    datos = ""
+
+    if len(lista) == 0:
+        datos = "No hay ningun paciente en espera."
+    else:
+        datos = "Lista de Espera: \n ------------------------------ \n"
+        for usuario in lista:
+            datos += "Cedula: " + usuario['cedula'] + "\n"
+            datos += "Nombre: " +usuario['nombre'] + "\n"
+            datos += "Apellido: " + usuario['apellido'] + "\n"
+            datos += "Edad: " + str(usuario['edad']) + "\n"
+            datos += "Sexo: " + usuario['sexo'] + "\n"
+            datos += "Numero de la cita: " + str(usuario['numero_cita']) + "\n"
+            datos += "Precio de la cita: " + str(usuario['total_cita']) + "\n"
+            datos += "Tratamiento: " + usuario['tratamiento'] + "\n"
+            datos += "Costo del tratamiento: " + str(usuario['costo_tratamiento']) + "\n---------------------- \n"
+
+    label_nombre = Label(ventana, text = datos)
+    label_nombre.grid(row = 3, column = 1, padx = 100, pady = 20)
